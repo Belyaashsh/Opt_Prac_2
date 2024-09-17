a2cb94550f774233b6b191cde26a4416e930cce7
vityaii <m2209008@edu.misis.ru>
2024-09-17 12:04:50 +0300

    Done Version 1.0.0

--- calc.py
+++ calc.py
@@ -6,14 +6,14 @@ class calc :
             if (order.elevator == True) :
                 return 300
             else :
-                return (300 * (order.floor - 1) + 2000)
+                return (300 * (order.floor - 1) + 300)
         if (order.weight > 50 and order.weight <= 100) :
             if (order.elevator == True) :
                 return 1000
             else :
-                return round(300 * (order.weight / 100) * (order.floor - 1) + 2000)
+                return round(300 * round(order.weight / 100) * (order.floor - 1) + 1000)
         if (order.weight > 100) :
             if (order.elevator == True) :
-                return 1200
+                return 2000
             else :
-                return round(300 * (order.weight / 100) * (order.floor - 1) + 2000)    
+                return round(300 * round(order.weight / 100) * (order.floor - 1) + 2000)    
