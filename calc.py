162b56af47f0dce62b7b3515e6dbea9e918f6b4b
vityaii <m2209008@edu.misis.ru>
2024-09-17 11:40:54 +0300

    Script for calculating price added

--- calc.py
+++ calc.py
@@ -1,6 +1,19 @@
 from order_generate import COrder
 
 class calc :
-    price = 0
     def Calculate_Price(self, order = COrder) :
-        
+        if (order.weight <= 50) :
+            if (order.elevator == True) :
+                return 300
+            else :
+                return (300 * (order.floor - 1) + 2000)
+        if (order.weight > 50 and order.weight <= 100) :
+            if (order.elevator == True) :
+                return 1000
+            else :
+                return (300 * (order.weight / 100) * (order.floor - 1) + 2000)
+        if (order.weight > 100) :
+            if (order.elevator == True) :
+                return 1200
+            else :
+                return (300 * (order.weight / 100) * (order.floor - 1) + 2000)    
